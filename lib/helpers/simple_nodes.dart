8d7ba6d0382efde5954236557add681aac00c53b
jawwad_hassan <jh@embrace-it.com>
2022-07-06 15:56:42 +0500

    add simple_nodes.dart

---
+++ lib/helpers/simple_nodes.dart
@@ -0,0 +1,27 @@
+import 'package:listenable_tree/listenable_tree.dart';
+
+class SimpleNode extends ListenableNode<SimpleNode> {
+  /// A [SimpleNode] that can be used with the [TreeView].
+  ///
+  /// To use your own custom data with [TreeView], extend the [ListenableNode]
+  /// like this:
+  /// ```dart
+  ///   class YourCustomNode extends ListenableNode<YourCustomNode> {
+  ///   ...
+  ///   }
+  /// ```
+  SimpleNode([String? key]) : super(key: key);
+}
+
+class SimpleIndexedNode extends ListenableIndexedNode<SimpleIndexedNode> {
+  /// A [SimpleIndexedNode] that can be used with the [IndexedTreeView].
+  ///
+  /// To use your own custom data with [IndexedTreeView], extend the [ListenableIndexedNode]
+  /// like this:
+  /// ```dart
+  ///   class YourCustomIndexedNode extends ListenableIndexedNode<YourCustomIndexedNode> {
+  ///   ...
+  ///   }
+  /// ```
+  SimpleIndexedNode([String? key]) : super(key: key);
+}
